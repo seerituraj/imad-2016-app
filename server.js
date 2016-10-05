34dded136b397bf850dfded6716eb7d64c1f4415
seerituraj <seerituraj@gmail.com>
2016-10-05 21:19:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.paras.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
